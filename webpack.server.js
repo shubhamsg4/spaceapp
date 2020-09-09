959efbb36e6b0b4363018241b343ea3b8903b5ad
shubhamsg4 <goelshubham64@gmail.com>
2020-09-09 23:06:51 +0530

    make it better

--- webpack.server.js
+++ webpack.server.js
@@ -6,20 +6,22 @@ const webpackNodeExternals = require('webpack-node-externals');
 const config = {
   // Inform webpack that we're building a bundle
   // for nodeJS, rather than for the browser
-  target: 'node',
+  target: "node",
+
+  mode: "production",
 
   // Tell webpack the root file of our
   // server application
-  entry: './src/index.js',
+  entry: "./src/index.js",
 
   // Tell webpack where to put the output file
   // that is generated
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
   },
 
-  externals: [webpackNodeExternals()]
+  externals: [webpackNodeExternals()],
 };
 
 module.exports = merge(baseConfig, config);
